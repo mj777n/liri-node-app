8f35c4fce5ad2271aa339c3c8600464cc750c6bd
M N <mj777norato@gmail.com>
2019-08-17 00:27:54 -0400

    working concert-this function

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 // Takes in all of the command line arguments
-var inputString = process.argv;
+// var inputString = process.argv;
   // Capture the command line argument into array var:
   //  using "command" options:
   //    "concert-this"
@@ -11,7 +11,8 @@ var title = process.argv[3];
 
 switch (command) {
   case "concert-this":
-    // search "BandsInTown API"
+    // search "BandsInTown API" using AXIOS
+    concertThis();
     break;  
   case "spotify-this-song":
     // search "Spotify-Songs"
@@ -19,8 +20,7 @@ switch (command) {
     spotifySongs();
     break;  
   case "movie-this":
-    // search OMDB using AXIOS
-    
+    // search OMDB using AXIOS   
     movieThis();
     break;  
   case "do-what-it-says":
@@ -28,6 +28,67 @@ switch (command) {
     break;
 }
 // ************** BEGIN FUNCTIONS ***********************8
+function concertThis() {
+  // Include the axios npm package
+  var axios = require("axios");
+    // Grab the artist name which will always be the third node argument.
+    // If no movie is entered, we'll print out a console log message for now
+  var artist = title;
+  if (title === undefined) {
+    console.log("Please enter an Artist to search for")
+  }
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    // This line is just to help us debug against the actual URL.
+  axios.get(queryUrl).then(
+    function(response) {
+      var results = response.data;
+      var hits = 0;
+      console.log("\n"+"These are the next shows nearest to you");
+      console.log("---------------------------------------------------------");
+      for (i = 0; i < results.length; i++) {
+        if (results[i].venue.region === "FL") {
+          hits  ++;
+          console.log("Band Name    : "+artist);
+          console.log("Venue        : "+results[i].venue.name);
+          console.log("City/State   : "+results[i].venue.city+", "+results[i].venue.region);
+          console.log("Date and Time: "+results[i].datetime); 
+          console.log("\n"+"--------------------------------------------------");
+        } else {
+          // console.log("There are no upcoming events in Florida for this group");
+        }      
+      }  // end for Loop
+      if (hits === 0){
+        console.log("Sorry, there were no shows found near you (In your State)");
+        console.log("---------------------------------------------------------")
+      }  
+        console.log("Number of shows in Florida = "+hits);  
+        console.log("Number of shows in the US  = "+i);  
+      
+    })  // end .then call back function (response)     
+
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+      console.log("IN THE ERROR LOG");
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+    console.log("IN THE 2nd ERROR LOG");
+  }); 
+}  // end function concertThis()
 
 function movieThis(){
     // Include the axios npm package
@@ -41,15 +102,17 @@ function movieThis(){
   var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
     // This line is just to help us debug against the actual URL.
   axios.get(queryUrl).then(
-    function(response) {
-      console.log("Title: "+response.data.Title)
+    function(response) {      
+      console.log("\n"+"------------------------------------");
+      console.log("Title: "+response.data.Title);
       console.log("Year Relased: "+response.data.Year);
       console.log("IMDB Rating: " + response.data.imdbRating);
       console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
       console.log("Country produced in: " + response.data.Country);
       console.log("Language of the movie: " + response.data.Language);
       console.log("Plot: " + response.data.Plot);
-      console.log("Actors: " + response.data.Actors);    
+      console.log("Actors: " + response.data.Actors);   
+      console.log("------------------------------------------") 
     })  // end .then call back function (response)
 
   .catch(function(error) {
@@ -71,9 +134,9 @@ function movieThis(){
       console.log("Error", error.message);
     }
     console.log(error.config);
-  });
+  })  // end call back .catch function
 }  // end function movieThis
-
+// **************************************************
 function spotifySongs() {
     var Spotify = require('node-spotify-api');
  
@@ -86,25 +149,19 @@ spotify
   .search({ type: 'track', query: title, limit: 10 })
   .then(function(response) {
   
-   for (i = 0; i < 10; i++) {
+  for (i = 0; i < 10; i++) {
     console.log("\n"+(i+1)+") -----------------------------");
     console.log("Artist: "+response.tracks.items[i].artists[0].name);  
     console.log("Song title: "+response.tracks.items[i].name);
     console.log("Preview link to track: "+response.tracks.items[i].preview_url);
     console.log("Album: "+response.tracks.items[i].album.name);   
   } // end for Loop
-  })
+  })  // end .then call-back function
+
   .catch(function(err) {
     console.log(err);
-  });
-
-// spotify.search({ type: 'track', query: 'Angie' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-// console.log(data); 
-// });
-}  // end function spotifySongs()
+  })
+}  // end of function spotifySongs()
 
 
 
@@ -114,8 +171,6 @@ spotify
 // with the dotenv package:
 
 // require("dotenv").config(); +++++++++++++++++
-
-
 
 // Add the code required to 
 // import the `keys.js` file 
